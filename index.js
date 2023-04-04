a101da32a916a8baec9f0a1e0eb0c0334b9bd3e5
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-04-04 16:09:26 -0400

    ADD endpoint and imported tvLibrary

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
 
+// import CRUD functions
+import { addNewTv,getAllTv } from "./src/tvLibrary.js";
+
 const PORT = 3016;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// get all TV shows
+app.get('/tvShows', getAllTv);
+// add TV shows
+app.post('tvShows', addNewTv);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
